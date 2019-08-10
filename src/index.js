9792fcabb1da56f9130d54ec9001a8ed303cae94
The_Sun <35347139+RodrigoBLima@users.noreply.github.com>
2019-08-10 18:00:25 -0300

    create component card

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import Card from "./Card";
 import "./styles.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      persons: []
+    };
   }
   componentWillMount() {
-    let INITIAL_URL = "ausausah";
-
+    let INITIAL_URL = "https://thesimpsonsquoteapi.glitch.me/quotes";
     axios.get(INITIAL_URL).then(function(response) {
       console.log(response.data);
+      this.setState({
+        persons: response.data
+      });
     });
   }
   render() {
     return (
       <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+      {this.state.persons.map(person)
+        <Card />
+      }
       </div>
     );
   }
